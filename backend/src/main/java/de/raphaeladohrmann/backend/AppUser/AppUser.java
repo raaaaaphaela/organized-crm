a467e80b64e20bcfdee436d6d5bb8b61ba75533a
Raphaela <raphaela.dohrmann@gmail.com>
2023-01-23 15:41:11 +0100

    feat: add authorization

---
+++ backend/src/main/java/de/raphaeladohrmann/backend/AppUser/AppUser.java
@@ -0,0 +1,17 @@
+package de.raphaeladohrmann.backend.AppUser;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUser {
+
+    private String id;
+    private String username;
+    private String password;
+    private String company;
+    private String role;
+}
