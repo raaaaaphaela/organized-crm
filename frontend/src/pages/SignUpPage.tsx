baff0450984e9c76fed41a7fdec5e50215d3b678
Raphaela <raphaela.dohrmann@gmail.com>
2023-01-26 14:31:07 +0100

    feat: MUI for SignUp

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,6 +1,9 @@
 import React, {FormEvent, useCallback, useState} from "react";
 import {useLocation, useNavigate} from "react-router-dom";
 import axios from "axios";
+import {Box, Button, TextField} from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 export default function SignUpPage() {
     const [credentials, setCredentials] = useState({
@@ -41,39 +44,61 @@ export default function SignUpPage() {
     );
 
     return (
-        <div className="signup__container">
-            <h1>Sign Up</h1>
-
+        <Box
+            sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+        >
+            <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+                <LockOutlinedIcon/>
+            </Avatar>
             {errors.length > 0 && (
                 <div>
                     {errors.map((error) => <p key={error}>{error}</p>)}
                 </div>
             )}
-
-            <form onSubmit={signUp}>
-                <div>
-                    <input
-                        placeholder={"username"}
-                        value={credentials.username}
-                        name={"username"}
-                        onChange={handleChange}
-                    />
-                </div>
-
-                <div>
-                    <input
-                        placeholder={"password"}
-                        type={"password"}
-                        name={"password"}
-                        value={credentials.password}
-                        onChange={handleChange}
-                    />
-                </div>
-
-                <div>
-                    <button className={"btn"}>Sign Up</button>
-                </div>
-            </form>
-        </div>
+            <Box component="form"
+                 onSubmit={signUp}
+                 sx={{
+                     mt: 1,
+                     width: 300,
+                 }}>
+                <TextField
+                    margin="dense"
+                    required
+                    fullWidth
+                    id="username"
+                    label="Username"
+                    name="username"
+                    value={credentials.username}
+                    autoComplete="username"
+                    autoFocus
+                    onChange={handleChange}
+                />
+                <TextField
+                    margin="dense"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    value={credentials.password}
+                    autoComplete="current-password"
+                    onChange={handleChange}
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{mt: 2, mb: 2}}
+                >
+                    Registrieren
+                </Button>
+            </Box>
+        </Box>
     )
 }
