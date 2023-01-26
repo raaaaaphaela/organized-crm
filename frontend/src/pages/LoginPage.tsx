3c076f1d5f4ebf2ff6293ee686eaab9625f0534a
Raphaela <raphaela.dohrmann@gmail.com>
2023-01-26 14:24:56 +0100

    feat: delete unnecessary divs

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -41,70 +41,66 @@ export default function LoginPage() {
     );
 
     return (
-        <div className={"login-page"}>
-            <div className={"login-page_container"}>
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
+        <Box
+            sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+        >
+            <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+                <LockOutlinedIcon/>
+            </Avatar>
+            <Typography component="h1">Login</Typography>
+            <Box component="form"
+                 onSubmit={login}
+                 sx={{
+                     mt: 1,
+                     width: 300,
+                 }}>
+                <TextField
+                    margin="dense"
+                    required
+                    fullWidth
+                    id="username"
+                    label="Username"
+                    name="username"
+                    value={credentials.username}
+                    autoComplete="username"
+                    autoFocus
+                    onChange={handleChange}
+                />
+                <TextField
+                    margin="dense"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    value={credentials.password}
+                    autoComplete="current-password"
+                    onChange={handleChange}
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{mt: 2, mb: 2}}
                 >
-                    <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
-                        <LockOutlinedIcon/>
-                    </Avatar>
-                    <Typography component="h1">Login</Typography>
-                    <Box component="form"
-                         onSubmit={login}
-                         sx={{
-                             mt: 1,
-                             width: 300,
-                         }}>
-                        <TextField
-                            margin="dense"
-                            required
-                            fullWidth
-                            id="username"
-                            label="Username"
-                            name="username"
-                            value={credentials.username}
-                            autoComplete="username"
-                            autoFocus
-                            onChange={handleChange}
-                        />
-                        <TextField
-                            margin="dense"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            value={credentials.password}
-                            autoComplete="current-password"
-                            onChange={handleChange}
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{mt: 2, mb: 2}}
-                        >
-                            Einloggen
-                        </Button>
-                        <Grid container>
-                            <Grid item>
-                                <Typography sx={{typography: 'body1'}}> Noch keinen Account?&nbsp;
-                                    <Link to={"/signup"}>
-                                        {"Hier registrieren."}
-                                    </Link>
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Box>
-            </div>
-        </div>
+                    Einloggen
+                </Button>
+                <Grid container>
+                    <Grid item>
+                        <Typography sx={{typography: 'body1'}}> Noch keinen Account?&nbsp;
+                            <Link to={"/signup"}>
+                                {"Hier registrieren."}
+                            </Link>
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Box>
     )
 }
