bb11b2841da42cf3f537c64484c9048e9ec24f02
Raphaela <raphaela.dohrmann@gmail.com>
2023-02-01 14:51:34 +0100

    feat: add toastify notifications after customer save/update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './_app.scss';
 import {BrowserRouter} from "react-router-dom";
+import 'react-toastify/dist/ReactToastify.css'
 import Root from "./Root";
 
 function App() {
