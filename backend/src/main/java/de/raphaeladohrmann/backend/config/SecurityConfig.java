1e86a6491ae81fe60e35e3bb78ef69014bb58d5d
Raphaela <raphaela.dohrmann@gmail.com>
2023-02-22 10:04:15 +0100

    fix: security config

--- backend/src/main/java/de/raphaeladohrmann/backend/config/SecurityConfig.java
+++ backend/src/main/java/de/raphaeladohrmann/backend/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                 .csrf().disable()
                 .httpBasic().and()
                 .authorizeHttpRequests()
-                .antMatchers(HttpMethod.POST, "/api/app-users")
+                .antMatchers(HttpMethod.POST, "/api/app-users/**")
                 .permitAll()
                 .antMatchers("/api/**")
                 .hasRole("BASIC")
